a138ea1501d5673d546af530faa5a066cb2548d8
openxw <openxw@qq.com>
2021-02-02 00:28:45 +0800

    修复跳转链接

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -15,9 +15,30 @@
       {{-- Right Side of Navbar  --}}
       <ul class="navbar-nav navbar-right">
         {{-- Authentication Links  --}}
+        @guest
         <li class="nav-item"><a href='{{ route('login') }}' class='nav-link'>登录</a></li>
-        <li class="nav-item"><a href='{{ route('logout') }}' class='nav-link'>注销</a></li>
+        <li class="nav-item"><a href='{{ route('register') }}' class='nav-link'>注册</a></li>
       </ul>
+        @else
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
+          {{ Auth::user()->name }}
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="">个人中心</a>
+          <a class="dropdown-item" href="">编辑资料</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" id="logout" href="#">
+              <form action="{{ route('logout') }}" method="POST">
+              {{ csrf_field() }}
+              <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+              </form>
+          </a>
+          </div>
+      </li>
+        @endguest
+
 
     </div>
   </div>
