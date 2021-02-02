4ad4dbec091ed983590d994d8a9edda32fea544a
openxw <openxw@qq.com>
2021-02-02 10:18:56 +0800

    登录验证码

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,38 +3,53 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
-    use AuthenticatesUsers;
-
     /**
-     * Where to redirect users after login.
+     * Handle an authentication attempt.
      *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
-
-    /**
-     * Create a new controller instance.
+     * @param  \Illuminate\Http\Request $request
      *
-     * @return void
+     * @return Response
      */
-    public function __construct()
+    public function showLoginForm()
     {
-        $this->middleware('guest')->except('logout');
+        # code...
+        return view('auth.login');
+    }
+
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'captcha' => ['required', 'captcha'],
+        ], [
+            'captcha.required' => '验证码不能为空',
+            'captcha.captcha' => '请输入正确的验证码',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('login')
+                        ->withErrors($validator)
+                        ->withInput();
+        }
+
+        $credentials = $request->only('email', 'password',);
+
+        if (Auth::attempt($credentials)) {
+            // 通过认证..
+            return redirect()->intended('/');
+        }
+    }
+
+    public function logout()
+    {
+        # code...
+        Auth::logout();
+        return redirect()->intended('/');
     }
 }
